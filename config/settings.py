f0a138c3f385da20e0facb99c4ed7ea32e7f3445
Ada Kravchenko <ada.t.krav@gmail.com>
2023-03-29 19:03:40 +0300

    add crispy_forms to project settings

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_bootstrap4",
+    "crispy_forms",
     "agency"
 ]
 
@@ -67,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 WSGI_APPLICATION = "config.wsgi.application"
 
