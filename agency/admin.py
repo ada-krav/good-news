4544d504c614d4d09fd0699cc50322b498340a44
Ada Kravchenko <ada.t.krav@gmail.com>
2023-03-29 16:00:13 +0300

    register models in admin panel

--- agency/admin.py
+++ agency/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from agency.models import Redactor, Article, Topic
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_filter = ("topic",)
+
+
+admin.site.register(Topic)
