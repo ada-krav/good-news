8cb2a300026a8475a5e72b662dd385525a239b39
Ada Kravchenko <ada.t.krav@gmail.com>
2023-03-29 16:54:32 +0300

    add index view and update agency.urls

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from agency.models import Redactor, Article, Topic
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+    num_redactors = Redactor.objects.count()
+    num_articles = Article.objects.count()
+    num_topics = Topic.objects.count()
+
+    contex = {
+        "num_redactors": num_redactors,
+        "num_articles": num_articles,
+        "num_topics": num_topics,
+    }
+
+    return render(request, "agency/index.html", context=contex)
