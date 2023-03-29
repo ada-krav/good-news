8cb669320272443e24beb3ccf99aebf9b63ce891
Ada Kravchenko <ada.t.krav@gmail.com>
2023-03-29 19:05:01 +0300

    add search by name to topic_list view & create corresponding form and template

--- agency/views.py
+++ agency/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 
+from agency.forms import TopicSearchForm
 from agency.models import Redactor, Article, Topic
 
 
@@ -26,6 +27,21 @@ class TopicListView(LoginRequiredMixin, generic.ListView):
     model = Topic
     context_object_name = "topic_list"
     template_name = "agency/topic_list.html"
-    queryset = Topic.objects.all()
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        contex = super(TopicListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        contex["search_form"] = TopicSearchForm(
+            initial={"name": name}
+        )
+        return contex
+
+    def get_queryset(self):
+        queryset = Topic.objects.all()
+        form = TopicSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+        return queryset
 
